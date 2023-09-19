ce0dec0a46854fe2b7fa38d43d32152840a81f66
istafamarshall <istafa.a.marshall@gmail.com>
2023-09-19 14:49:07 -0400

    create handleResponse function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import './App.css';
+import jwtDecode from "jwt-decode";
 
 function App() {
 
-  function handleResponse(respsonse) {
-    
+  const [user, setUser] = useState([])
+
+  function handleResponse(response) {
+    console.log("JWT ID token" + response.credential);
+    let Token = jwtDecode(response.credential)
+    console.log(Token)
   }
 
   useEffect(() => {
     /* global google */
     google.accounts.id.initialize({
-      client_id: "",
+      client_id: "403940930490-9p5upakgv7g8brgiignanr4frs6r3rsv.apps.googleusercontent.com",
       callback: handleResponse
     })
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "outline", size: "large"}
+    );
   }, []);
 
   return (
     <div className="App">
       <h1>Google Login React App</h1>
+
+      <div id="signInDiv" ></div>
     </div>
   );
 }
