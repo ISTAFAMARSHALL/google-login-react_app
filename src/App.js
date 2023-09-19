a8e6522520c6cd1d5a23db7ed316f8e3a4ed0ca9
istafamarshall <istafa.a.marshall@gmail.com>
2023-09-19 15:23:54 -0400

    create handleSignout function

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import jwtDecode from "jwt-decode";
 
 function App() {
 
-  const [user, setUser] = useState([])
+  const [login, setLogin] = useState(true)
+  const [user, setUser] = useState()
 
   function handleResponse(response) {
     console.log("JWT ID token" + response.credential);
     let Token = jwtDecode(response.credential)
     console.log(Token)
+    setUser(Token)
+    setLogin(false)
+    document.getElementById("signInDiv").hidden = true
+  }
+  function handleSignout(e) {
+    setLogin(true)
+    document.getElementById("signInDiv").hidden = false
+    setUser([])
   }
 
   useEffect(() => {
@@ -23,13 +32,25 @@ function App() {
       document.getElementById("signInDiv"),
       {theme: "outline", size: "large"}
     );
-  }, []);
+
+    google.accounts.id.prompt();
+
+  }, [user]);
 
   return (
     <div className="App">
       <h1>Google Login React App</h1>
-
-      <div id="signInDiv" ></div>
+      {login && <div id="signInDiv" ></div>}
+      {!login &&
+        <div>
+          <img src={user.picture} alt="user.name"></img>
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+          <br></br>
+          <button onClick={ (e)=> handleSignout(e)}>Sign out</button>
+        </div>
+      }
+      
     </div>
   );
 }
